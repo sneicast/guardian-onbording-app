a67cd12d555c0111b76b556af7c935817f42293f
Sneider Castillo <sneiderz15@gmail.com>
2025-11-21 03:36:39 -0500

    add validate token

---
+++ guardian-onbording-backend/src/common/guards/jwt-auth/jwt-auth.guard.ts
@@ -0,0 +1,16 @@
+import {  Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+    handleRequest(err: any, user: any, info: any) {
+        if (err || !user) {
+          throw new UnauthorizedException({
+            statusCode: 401,
+            message: 'No estás autorizado. Debes enviar un token JWT válido.',
+            error: 'Unauthorized',
+          });
+        }
+        return user;
+      }
+}
--- guardian-onbording-backend/src/modules/onboarding/onboarding.controller.ts
+++ guardian-onbording-backend/src/modules/onboarding/onboarding.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { OnboardingService } from './onboarding.service';
 import { OnboardingCreateDto } from './dtos/onboarding-create.dto';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth/jwt-auth.guard';
 
 @Controller('onboarding')
 export class OnboardingController {
     constructor(private readonly onboardingService: OnboardingService){}
 
+    @UseGuards(JwtAuthGuard)
     @Post()
     createOnboarding(@Body() request: OnboardingCreateDto) {
         return this.onboardingService.createOnboarding(request);
